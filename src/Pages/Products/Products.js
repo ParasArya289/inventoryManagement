42d2d3df6e4e759e99a3d1321d7b398fabdf2bda
Paras Arya <parasarya289@gmail.com>
2023-08-12 21:15:11 +0530

    feat: filter header

--- src/Pages/Products/Products.js
+++ src/Pages/Products/Products.js
@@ -2,21 +2,64 @@ import { Layout } from "../../Components/Layout/Layout";
 import { useData } from "../../Context/dataContext";
 import "./Products.css";
 
-export const Products = () => {
+export const FilterHeader = () => {
   const {
     filters,
+    setFilters,
     dataState: { departments },
   } = useData();
+  const filterChangeHandler = (e) => {
+    const { name, value, type, checked } = e.target;
+    if (type === "checkbox") {
+      setFilters((prev) => ({
+        ...prev,
+        [name]: checked,
+      }));
+      return;
+    }
+    setFilters((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+  return (
+    <header className="filterheader">
+      <select
+        value={filters.category}
+        name="category"
+        onChange={filterChangeHandler}
+      >
+        <option value="all">all</option>
+        {departments?.map((department) => (
+          <option value={department}>{department}</option>
+        ))}
+      </select>
+
+      <label>
+        <input
+          type="checkbox"
+          checked={filters.lowStockItem}
+          name="lowStockItem"
+          onChange={filterChangeHandler}
+        />
+        Low Stock Items
+      </label>
+
+      <select value={filters.sort} name="sort" onChange={filterChangeHandler}>
+        <option value="name">Name</option>
+        <option value="price">Price</option>
+        <option value="stock">Stock</option>
+      </select>
+    </header>
+  );
+};
+
+export const Products = () => {
   return (
     <Layout>
       <div>
         <h3>Products</h3>
-        <select value={filters.category}>
-          <option>all</option>
-          {departments?.map((department) => (
-            <option value={department}>{department}</option>
-          ))}
-        </select>
+        <FilterHeader />
       </div>
     </Layout>
   );
